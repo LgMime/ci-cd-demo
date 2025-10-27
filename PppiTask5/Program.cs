e2cb7c360d1ddfb515e7fe88bf8bede3d1ead1c2
LGKaleka <Bizubik228@gmail.com>
2025-10-27 22:52:27 +0200

    Add Armor

--- PppiTask5/Program.cs
+++ PppiTask5/Program.cs
@@ -1,19 +1,22 @@
 ﻿//you choose the smith not a sword
 
 using ForgeMaster;
+using PppiTask5.ForgeMaster;
 
-NorwaySmith norwaySmith = new NorwaySmith(new FactorySword());
-JapanSmith japanSmith = new JapanSmith(new FactorySword());
+NorwaySmith norwaySmith = new NorwaySmith(new FactorySword(), new FactoryArmor());
+JapanSmith japanSmith = new JapanSmith(new FactorySword(), new FactoryArmor());
 Console.WriteLine("We have, a smiths Norway & Japan:"); ///ask user to choose smith
-Console.Write($"Selected Smith: "); //on this line user inputs Norway or Japan, because here Write not WriteLine
+Console.Write($"Selected Smith: "); //its line where user inputs smith and sees text Selected Smith:
 string selectedSmith = Console.ReadLine(); //input Norway or Japan
 if (selectedSmith == "Norway")
 {
     norwaySmith.ForgeSword();
+    norwaySmith.ForgeArmor();
 }
 else if (selectedSmith == "Japan")
 {
     japanSmith.ForgeSword();
+    japanSmith.ForgeArmor();
 }
 else
 {
